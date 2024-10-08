3a4b1812786daf639406c451a01084813f6dda15
AstronautDesh <powerfistmarley@gmail.com>
2024-10-08 09:17:47 +0100

    homePage product display

---
+++ client/src/Data/salesPitchData.js
@@ -0,0 +1,82 @@
+// src/salesPitchData.js
+import gShock from "../Asset/Product/G-shock-red.webp";
+import gShockblac from "../Asset/Product/G-shock-blac.webp";
+import garmin from '../Asset/Product/garmin.webp';
+import garmininepix from '../Asset/Product/garminepix.webp';
+import euronics from '../Asset/Product/euronicz.webp';
+import huaweiPearl from '../Asset/Product/huawei-pearl-white.webp';
+import huaweiWatch from '../Asset/Product/huawei-watch-buds.webp';
+import snapFit from '../Asset/Product/snap-fitness-watch.webp';
+import huaweiFitSpecial from '../Asset/Product/huaweiFitSpecial.webp';
+export const salesPitchData = [
+  {
+    id: 1,
+    image: gShock,
+    rating: "11",
+    price: "N34,500",
+    description: "G-Shock G-Rescue Red Dial Men's Watch - Shock Resistant, Moon/Tide Graphs, World Time, Chronograph - 200m Water Resistant"
+  },
+  {
+    id: 2,
+    image: gShock,
+    rating: "9",
+    price: "N29,999",
+    description: "Another G-Shock model with different features and specifications"
+  },
+  {
+    id: 3,
+    image: gShockblac,
+    rating: "9",
+    price: "N59,999",
+    description: "Another G-Shock model with different features and specifications"
+  },
+  {
+    id: 4,
+    image: garmin,
+    rating: "8.5",
+    price: "N63,999",
+    description: "Another G-Shock model with different features and specifications"
+  },
+  {
+    id: 5,
+    image: garmininepix,
+    rating: "8.0",
+    price: "N61,509",
+    description: "Another G-Shock model with different features and specifications"
+  },
+  {
+    id: 6,
+    image: euronics,
+    rating: "5.5",
+    price: "N37,319",
+    description: "From The Stable Of House Euronics...A Timepiece"
+  },
+  {
+    id: 7,
+    image: huaweiPearl,
+    rating: "6.0",
+    price: "N33,019",
+    description: "Huawei Pearl White"
+  },
+  {
+    id: 8,
+    image: huaweiFitSpecial,
+    rating: "7.0",
+    price: "N43,019",
+    description: "Huawei Fit Special"
+  },
+  {
+    id: 9,
+    image: snapFit,
+    rating: "6.0",
+    price: "N33,019",
+    description: "Snap Fitness Watch"
+  },
+  {
+    id: 10,
+    image: huaweiWatch,
+    rating: "5.5",
+    price: "N25,019",
+    description: "Huawei Watch Buds"
+  },
+];
--- client/src/Pages/HomePage.js
+++ client/src/Pages/HomePage.js
@@ -2,15 +2,15 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { motion } from "framer-motion";
 import "../css/homePage.css";
-import gShock from "../Asset/G-shock-red.webp";
+import { salesPitchData } from "../Data/salesPitchData";
 
 function HomePage() {
   return (
     <div className="homePage">
       <motion.div className="scroll-container">
-        {[1, 2].map((index) => (
+        {salesPitchData.map((pitch) => (
           <motion.div
-            key={index}
+            key={pitch.id}
             className="sales-pitch"
             initial={{ opacity: 0 }}
             whileInView={{ opacity: 1 }}
@@ -24,24 +24,23 @@ function HomePage() {
                   xs={12}
                   className="watch-grid d-flex justify-content-center align-items-center"
                 >
-                  <img src={gShock} className="img-box" alt="wrist-watch" />
+                  <img src={pitch.image} className="img-box" alt="wrist-watch" />
                 </Col>
                 <div className="sales-pitch-text">
                   <Col xs={12}>
                     <Row>
                       <div className="box-grid">
                         <Col xs={5} md={6} className="box1">
-                          <p className="rating">11</p>
+                          <p className="rating">{pitch.rating}</p>
                         </Col>
                         <Col xs={5} md={6} className="box2">
-                          <h3 className="price">N34,500</h3>
+                          <h3 className="price">{pitch.price}</h3>
                         </Col>
                       </div>
                     </Row>
                   </Col>
                   <Col xs={12} className="desc">
-                    G-Shock G-Rescue Red Dial Men's Watch - Shock Resistant, Moon/Tide
-                    Graphs, World Time, Chronograph - 200m Water Resistant
+                    {pitch.description}
                   </Col>
                 </div>
               </Row>
